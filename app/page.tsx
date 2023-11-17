964d6052362a18ad468809cd03a70cde37025938
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-17 14:35:39 -0600

    Fixed the OGP tag properties

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
       <Head>
         <meta
           property="og:image"
-          content={dogImage.src}
+          content= "https://nextjs-mesh-seven.vercel.app/dog.JPG"
         />
         <meta property="og:description" content="Dog website" />
-        <meta property="og:image:type" content="/public/dog.jpg" />
-        <meta property="og:image:width" content="600" />
-        <meta property="og:image:height" content="500" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="919" />
+        <meta property="og:image:height" content="1280" />
         <meta property="og:type" content="website" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
