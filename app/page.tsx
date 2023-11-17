7ed6d372384a20c9cd47db0fdc2e020b9c2f1613
Izak Robles <izakrobles@gmail.com>
2023-11-16 20:26:39 -0600

    oimage

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
+      <title>Mesh.</title>
       <Head>
-        <title>Mesh.</title>
         <meta
           property="og:image"
-          content="https://images.squarespace-cdn.com/content/v1/5d016c1ba9f2e7000120c08c/1560579210527-7LDCZH62S91OLOJIKAE9/AdobeStock_87517185.jpeg?format=1000w"
+          content="https://nextjs-mesh-seven.vercel.app/dog.JPG"
         />
         <meta property="og:description" content="Dog website" />
         <meta property="og:image:type" content="image/jpeg" />
