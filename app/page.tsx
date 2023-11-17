19ddba27c7c5e18005b04c9e8a220e4b677b302c
Izak Robles <izakrobles@gmail.com>
2023-11-16 20:20:17 -0600

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <title>Mesh.</title>
       <Head>
+        <title>Mesh.</title>
         <meta
           property="og:image"
           content="https://images.squarespace-cdn.com/content/v1/5d016c1ba9f2e7000120c08c/1560579210527-7LDCZH62S91OLOJIKAE9/AdobeStock_87517185.jpeg?format=1000w"
@@ -16,6 +16,7 @@ export default function Home() {
         <meta property="og:type" content="website" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Mesh. Four People Together</h1>
       </main>
     </>
   );
