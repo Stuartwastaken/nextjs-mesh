c8144e5acb5e2ce2d4c0486ee9b6c8f897c3553e
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-17 09:35:48 -0600

    Stylized the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
 import Image from 'next/image';
+import qrCodeImage from '../public/qr_code_barcode.png';
 
 export default function Home() {
   return (
-    <main className="bg-black min-h-screen text-white flex flex-col items-center justify-center p-8">
+    <main className="bg-black min-h-screen text-white flex flex-col items-center justify-center px-8">
       {/* Early Access Tag */}
       <div className="triangleTag"></div>
       
-      {/* Mesh Text in Custom Font */}
-      <h1 className="customFont text-4xl">Mesh</h1>
+      <h1 className="customFont text-6xl font-bold mb-12 uppercase">Mesh</h1> {/* Larger and bolder font size, increased spacing */}
 
-      {/* Center QR Code Placeholder - Add your QR Code here */}
-      <div className="flex flex-col items-center justify-center">
-        <p className="mt-4 text-center">
-          PLEASE SCAN ON YOUR MOBILE
-        </p>
+      {/* Center QR Code */}
+      <div className="flex flex-col items-center justify-center mb-12"> {/* Increased bottom margin */}
+        <Image
+          src={qrCodeImage}
+          alt="QR Code"
+          width={600} 
+          className="mb-4" 
+        />
       </div>
+
+
+      <p className="text-xl font-light"> 
+        PLEASE SCAN ON YOUR MOBILE
+      </p>
     </main>
   )
 }
