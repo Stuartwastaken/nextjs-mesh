8c9e34d488d6d290a98bef0f94c7ac0b10caf298
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-30 17:33:34 -0600

    Going to try with context having issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
 import qrCodeImage from "../public/qr_code_barcode.png";
-import { useRouter } from "next/router"; // Import the useRouter hook
-import Head from 'next/head'; // Make sure to import Head for setting head elements
+import Head from 'next/head';
 
-export default function Home() {
-  const router = useRouter(); // Initialize the useRouter hook
-  // Extract the username from the query parameters
-  const { username } = router.query;
+type HomeProps = {
+  username: string;
+};
 
-  // If there's no username, you can decide to set a default one or handle it differently
-  const defaultUsername = 'defaultUsername';
-  const effectiveUsername = username || defaultUsername;
-
+export default function Home({ username }: HomeProps) {
   const baseUrl = 'https://nextjs-mesh-seven.vercel.app/';
-  const ogImageUrl = `${baseUrl}/api/og?username=${effectiveUsername}`;
+  const ogImageUrl = `${baseUrl}/api/og?username=${encodeURIComponent(username)}`;
 
   return (
     <>
@@ -39,4 +34,16 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Extract the username from the query parameters
+  const { query } = context;
+  const username = query.username || 'defaultUsername'; // Provide a default username if none is provided
+  
+  return {
+    props: {
+      username,
+    },
+  };
 }
