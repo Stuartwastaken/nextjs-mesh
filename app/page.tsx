099dc6a9c2c0aaf5a1e793b9e17fc671e134995b
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-30 17:04:01 -0600

    Pushing this to main

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 import qrCodeImage from "../public/qr_code_barcode.png";
 
 export default function Home() {
-  const ogImageUrl = 'https://https://nextjs-mesh-seven.vercel.app/api/og';
+  const baseUrl = 'https://nextjs-mesh-seven.vercel.app/'
+  const ogImageUrl = `${baseUrl}/api/og?username=defaultUsername`;
 
   return (
     <>
       <title>Mesh.</title>
       <meta property="og:title" content="Mesh. Four People Together" />
-      <meta property="og:description" content="Dog website" />
-      <meta property="og:image" content={ogImageUrl} />
+      <meta property="og:description" content="Connect and collaborate on Mesh." />
+      <meta property="og:image" content={ogImageUrl} />ç
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="919" />
-      <meta property="og:image:height" content="1280" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta property="og:type" content="website" />
       <main className="bg-black min-h-screen text-white flex flex-col items-center justify-center px-8 p-48">
         <div className="triangleTag" />
