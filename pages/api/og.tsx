9b1220a4f259bd364b6f5b88a982b66899335577
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-30 17:13:27 -0600

    Added a try catch to find the image

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,49 +1,63 @@
 import { ImageResponse } from 'next/og';
-// App router includes @vercel/og.
-// No need to install it.
- 
+
 export const runtime = 'edge';
- 
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const username = searchParams.get('username');
+
   if (!username) {
     return new ImageResponse(<>Visit with &quot;?username=vercel&quot;</>, {
       width: 1200,
       height: 630,
     });
   }
- 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: 'flex',
-          fontSize: 60,
-          color: 'black',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          paddingTop: 50,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img
-          width="256"
-          height="256"
-          src={`https://github.com/${username}.png`}
+
+  try {
+    const res = await fetch(`https://github.com/${username}.png`);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch GitHub image for user ${username}`);
+    }
+    const imageBuffer = await res.arrayBuffer();
+    // If image fetch is successful, proceed to return the image response
+    return new ImageResponse(
+      (
+        <div
           style={{
-            borderRadius: 128,
+            display: 'flex',
+            fontSize: 60,
+            color: 'black',
+            background: '#f6f6f6',
+            width: '100%',
+            height: '100%',
+            paddingTop: 50,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
-        />
-        <p>github.com/{username}</p>
-      </div>
-    ),
-    {
+        >
+          <img
+            width="256"
+            height="256"
+            src={`https://github.com/${username}.png`}
+            style={{
+              borderRadius: 128,
+            }}
+          />
+          <p>github.com/{username}</p>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch (error) {
+    console.error(error);
+    // Return an error image response or handle the error as appropriate
+    return new ImageResponse(<>Error: Failed to fetch image for user {username}</>, {
       width: 1200,
       height: 630,
-    },
-  );
+    });
+  }
 }
