8f3ca789d315ca80cf950884e69184fd57226c6e
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-12-01 16:37:13 -0600

    Blurred front image but not background

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -17,42 +17,57 @@ export default async function handler(req: Request) {
   }
 
   try {
-    const res = await fetch(`https://github.com/${username}.png`);
-    if (!res.ok) {
-      throw new Error(`Failed to fetch GitHub image for user ${username}`);
-    }
+    const imageUrl = `https://github.com/${username}.png`;
+    const res = await fetch(imageUrl);
+    if (!res.ok) throw new Error(`Failed to fetch image for user ${username}`);
+    
+    const buffer = await res.arrayBuffer();
+    const base64 = Buffer.from(buffer).toString('base64');
+    const dataUrl = `data:image/png;base64,${base64}`;
     // Proceed to return the image response
     return new ImageResponse(
       (
         <div
           style={{
+            position: 'relative',
             display: 'flex',
-            fontSize: 60,
-            color: 'black',
-            background: '#f6f6f6',
-            width: '100%',
-            height: '100%',
-            paddingTop: 50,
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
+            width: '100%',
+            height: '100%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundImage: `url('${dataUrl}')`,
+            filter: 'blur(8px)', // Apply blur to the background image
           }}
         >
+          <div
+            style={{
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              background: 'rgba(0, 0, 0, 0.5)', // Optional: overlay to increase text contrast
+            }}
+          />
           <img
             width="256"
             height="256"
-            src={`https://github.com/${username}.png`}
+            src={imageUrl}
             style={{
-              borderRadius: 128,
+              borderRadius: '50%',
+              position: 'relative', // Ensure the image is above the overlay
+              zIndex: 1,
             }}
           />
-          <p>github.com/{username}</p>
+          {/* Other content */}
         </div>
       ),
       {
         width: 1200,
         height: 630,
-      },
+        // Define additional properties if needed
+      }
     );
   } catch (error) {
     console.error(error);
