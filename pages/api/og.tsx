af75cf5bc9312d70f780aad3e01565b886394c33
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2025-01-12 17:13:04 -0600

    added a fallback for images tht are too big

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -114,7 +114,7 @@ export default async function handler(req: NextRequest) {
     fontData = await fetch(new URL("../../public/TYPEWR__.ttf", import.meta.url)).then(
       (res) => res.arrayBuffer()
     );
-    console.log("[OGP] Font loaded (byteLength):", fontData.byteLength);
+    console.log("[OGP] Font loaded:", fontData);
   } catch (fontError) {
     console.warn("[OGP] Could not load custom font. Continuing without it.", fontError);
   }
